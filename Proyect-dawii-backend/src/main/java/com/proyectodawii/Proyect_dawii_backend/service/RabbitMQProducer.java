442ae80f85b11a6b33ffdf75684b5db673f40f33
yair22 <yaircaballero10@hotmail.com>
2025-07-01 18:22:03 -0500

    agregando cambios

---
+++ Proyect-dawii-backend/src/main/java/com/proyectodawii/Proyect_dawii_backend/service/RabbitMQProducer.java
@@ -0,0 +1,22 @@
+package com.proyectodawii.Proyect_dawii_backend.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.proyectodawii.Proyect_dawii_backend.config.RabbitMQConfig;
+
+@Service
+public class RabbitMQProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_NAME,
+                RabbitMQConfig.ROUTING_KEY,
+                message
+        );
+        System.out.println("Mensaje enviado: " + message);
+    }
+}
