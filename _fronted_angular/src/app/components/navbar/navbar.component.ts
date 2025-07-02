fed3eabc48b9265db95517fbc34799e719592fc4
yair22 <yaircaballero10@hotmail.com>
2025-07-02 14:21:36 -0500

    cambios de fernando

--- _fronted_angular/src/app/components/navbar/navbar.component.ts
+++ _fronted_angular/src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, RouterModule,],
 })
 export class NavbarComponent {
-  constructor(public tokenService: TokenService){
-    
-  }
+  mostrarLogout = false;
+  constructor(public tokenService: TokenService, private router: Router) { }
 
+
+  toggleLogout() {
+    this.mostrarLogout = !this.mostrarLogout;
+  }
   logout() {
     this.tokenService.clear();
+    this.mostrarLogout = false;
+    this.router.navigate(['/']);
   }
 }
